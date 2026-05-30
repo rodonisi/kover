e27faede20ad72534f1069243d18fc97658c7b67
rodonisi <simon.rodoni@gmail.com>
2026-05-30 21:51:08 +0200

    map values on fetch

--- lib/mapping/dto/library_dto_mappings.dart
+++ lib/mapping/dto/library_dto_mappings.dart
@@ -2,13 +2,19 @@ import 'package:drift/drift.dart';
 import 'package:kover/api/openapi.swagger.dart' hide LibraryType;
 import 'package:kover/database/app_database.dart';
 import 'package:kover/models/enums/library_type.dart';
+import 'package:kover/utils/extensions/date_time.dart';
 
 extension LibraryDtoMappings on LibraryDto {
   LibrariesCompanion toLibrariesCompanion() {
-    return LibrariesCompanion(
+    return LibrariesCompanion.insert(
       id: Value(id!),
-      name: Value(name!),
-      type: Value(LibraryType.fromDtoType(type!)),
+      name: name!,
+      type: LibraryType.fromDtoType(type!),
+      includeInDashboard: Value.absentIfNull(includeInDashboard),
+      includeInRecommended: Value.absentIfNull(includeInRecommended),
+      includeInSearch: Value.absentIfNull(includeInSearch),
+      defaultLanguage: Value.absentIfNull(defaultLanguage),
+      lastScanned: Value.absentIfNull(lastScanned?.normalizeUtc()),
     );
   }
 }
