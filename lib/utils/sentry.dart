9a3949e709d20900d1277748c18818fdaea10588
rodonisi <simon.rodoni@gmail.com>
2026-06-11 23:18:05 +0200

    chore: pull sentry dsn from vars

--- lib/utils/sentry.dart
+++ lib/utils/sentry.dart
@@ -2,8 +2,7 @@ import 'dart:async';
 
 import 'package:sentry_flutter/sentry_flutter.dart';
 
-const _sentryDsn =
-    'https://b5a6b68eea23284eb215f1661c8661e2@o4511480670060544.ingest.de.sentry.io/4511480676679760';
+const _sentryDsn = String.fromEnvironment('SENTRY_DSN', defaultValue: '');
 
 Future<void> initializeSentry({
   required FutureOr<void> Function() appRunner,
