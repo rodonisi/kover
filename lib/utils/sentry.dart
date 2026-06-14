e3864e4351c6c5be62a089381e577f3d5fc38689
rodonisi <simon.rodoni@gmail.com>
2026-06-14 18:31:26 +0200

    chore: more scrubbing

--- lib/utils/sentry.dart
+++ lib/utils/sentry.dart
@@ -24,19 +24,38 @@ FutureOr<void> sentryOptionsConfiguration(SentryFlutterOptions options) {
   options.replay.sessionSampleRate = 0.1;
   options.replay.onErrorSampleRate = 1.0;
   options.beforeSend = (event, hint) {
-    final exceptions = event.exceptions;
-    if (exceptions != null) {
-      for (final exception in exceptions) {
-        final value = exception.value;
-        if (value != null) {
-          // scrub URLs from exception messages
-          exception.value = value.replaceAll(
-            RegExp(r'https?://[^\s,]+'),
-            '[scrubbed]',
-          );
-        }
-      }
-    }
+    _scrubExceptions(event.exceptions ?? []);
+    _scrubMessage(event.message);
     return event;
   };
+}
+
+void _scrubExceptions(List<SentryException> exceptions) {
+  for (final exception in exceptions) {
+    exception.scrubUrls();
+  }
+}
+
+void _scrubMessage(SentryMessage? message) {
+  message?.scrubUrls();
+}
+
+String _scrubLooseUrls(String input) {
+  final looseUrlRegex = RegExp(
+    r'(?:https?:\/\/|www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{2,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&//=]*)',
+    caseSensitive: false,
+  );
+  return input.replaceAll(looseUrlRegex, '[scrubbed]');
+}
+
+extension on SentryException {
+  void scrubUrls() {
+    value = _scrubLooseUrls(value ?? '');
+  }
+}
+
+extension on SentryMessage {
+  void scrubUrls() {
+    formatted = _scrubLooseUrls(formatted);
+  }
 }
